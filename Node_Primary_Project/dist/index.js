bb84d778255f9e520b2964fa41b4baa0373c5195
wellington197 <fcowellcosta@gmail.com>
2022-02-02 21:22:17 -0300

    Create project NodeJs

---
+++ Node_Primary_Project/dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+let nome = 'wellington';
+let idade = 34;
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos`);
---
+++ Node_Primary_Project/src/index.ts
@@ -0,0 +1,5 @@
+let nome: string = 'wellington';
+let idade: number = 34;
+
+
+console.log(`Meu nome é ${nome} e eu tenho ${idade} anos`);
